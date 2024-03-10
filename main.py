ec68f69c80e6e62d89ea54b5efdbce3cb7791b3d
Mick <moritonel@gmail.com>
2024-03-10 19:36:26 +0100

    added some values to __init__ from game class

--- main.py
+++ main.py
@@ -6,5 +6,18 @@ from pygame.locals import *
 
 class Game:
     def __init__(self):
-        pass
+        # setting up needed values
+        self.XO = "x"
+        self.winner = None
+        self.draw = None
+
+        # setting up game window width, height, 
+        # background color and color of the lines dviding board
+        self.width = 400
+        self.height = 400
+        self.white = (255, 255, 255)
+        self.line_color = (0, 0, 0)
+
+        #setting up a 3 * 3 board in canvas
+        self.board = [[None]*3, [None]*3, [None]*3]
 
