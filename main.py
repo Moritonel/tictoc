3e1100c6d35f061386cc7b4ef2c65a3485c5d513
Mick <moritonel@gmail.com>
2024-03-13 04:12:32 +0100

    added game_window method

--- main.py
+++ main.py
@@ -21,3 +21,31 @@ class Game:
         #setting up a 3 * 3 board in canvas
         self.board = [[None]*3, [None]*3, [None]*3]
 
+        #setting up the fps
+        self.fps = 30
+
+    def game_window(self):
+        #initializing the pygame window
+        pg.init()
+
+        # constant used to track the time
+        CLOCK = pg.time.Clock()
+
+        #build infrastructure of the display
+        screen = pg.display.set_mode((self.width, self.height + 100), 0, 32)
+
+        #nametag for the game window
+        pg.display.set_caption("Try not to suck at Tic Tac Toe")
+
+        #loading the images as obj
+        initiating_window = pg.image.load("modified_cover.png")
+        x_img = pg.image.load("X_modified.png")
+        y_img = pg.image.load("o_modified.png")
+
+        #resizing images
+        initiating_window = pg.transform.scale(initiating_window, (self.width, self.height +100))
+        x_img = pg.transform.scale(x_img, (80, 80))
+        o_img = pg.transform.scale(o_img, (80, 80))
+
+
+
